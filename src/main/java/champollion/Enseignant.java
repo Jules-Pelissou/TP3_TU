2bdb4fed1196ba3eecd7bbbf8f1c3368d2c587df
Jules Pelissou <113369857+Jules-Pelissou@users.noreply.github.com>
2024-11-20 12:10:54 +0100

    Update Enseignant.java

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,5 +1,12 @@
 package champollion;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ServiceConfigurationError;
+
 /**
  * Un enseignant est caractérisé par les informations suivantes : son nom, son adresse email, et son service prévu,
  * et son emploi du temps.
@@ -8,9 +15,22 @@ public class Enseignant extends Personne {
 
     // TODO : rajouter les autres méthodes présentes dans le diagramme UML
 
+    // Attribut
+    private Map<UE, Intervention> interventions;
+
+    private Map<UE, ServicePrevu> EDT;
+
     public Enseignant(String nom, String email) {
         super(nom, email);
+        this.EDT = new HashMap<UE, ServicePrevu>();
+        this.interventions = new HashMap<UE, Intervention>();
     }
+
+
+    public Map<UE, Intervention> getInterventions() {
+        return interventions;
+    }
+
 
     /**
      * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
@@ -22,7 +42,12 @@ public class Enseignant extends Personne {
      */
     public int heuresPrevues() {
         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        int total = 0;
+        for (ServicePrevu sp : EDT.values()) {
+            total += (int) ((sp.volumeCM +1.5) + sp.volumeTD + (sp.volumeTD*0.75));
+        }
+        return total;
+        //throw new UnsupportedOperationException("Pas encore implémenté");
     }
 
     /**
@@ -36,7 +61,13 @@ public class Enseignant extends Personne {
      */
     public int heuresPrevuesPourUE(UE ue) {
         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        int total = 0;
+        ServicePrevu prev = EDT.get(ue);
+        if (prev != null) {
+            total += (int) ((prev.volumeCM *1.5 ) + prev.volumeTD + (prev.volumeTP *0.75));
+        }
+        return total;
+        //throw new UnsupportedOperationException("Pas encore implémenté");
     }
 
     /**
@@ -49,7 +80,57 @@ public class Enseignant extends Personne {
      */
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        ServicePrevu sp = EDT.get(ue);
+        if (sp == null) {
+            sp = new ServicePrevu(ue, volumeCM, volumeTD, volumeTP);
+            EDT.put(ue, sp);
+        }else{
+            sp.setVolumeCM(volumeCM + sp.volumeCM);
+            sp.setVolumeTD(volumeTD + sp.volumeTD);
+            sp.setVolumeTP(volumeTP + sp.volumeTP);
+        }
+
+        //throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+
+    public void ajouteIntervention(String date, int duree, int h_deb, UE ue) {
+        ArrayList<Intervention> tabi = new ArrayList<Intervention>();
+        tabi.add(interventions.get(ue));
+        if (tabi.isEmpty()) {
+            Intervention i = new Intervention(date, duree, h_deb, ue);
+            interventions.put(ue, i);
+        }else{
+            int totalTmpsInterv = 0;
+            int totalTmpsEDT = heuresPrevuesPourUE(ue);
+            for (Intervention interv : interventions.values()){
+                totalTmpsInterv += interv.getDuree();
+            }
+            if (totalTmpsInterv < totalTmpsEDT && (totalTmpsInterv + duree) < totalTmpsEDT) {
+                Intervention i = new Intervention(date, duree, h_deb, ue);
+                interventions.put(ue, i);
+            }else{
+                throw new IllegalArgumentException("Le temps d'intervention dépasse le temps prévu à l'EDT");
+            }
+        }
+    }
+
+    public int resteAPlanifier(UE ue){
+        int totalTmpsEDT = heuresPrevuesPourUE(ue);
+        int totalTmpsInterv = 0;
+        Intervention i = interventions.get(ue);
+        for (Intervention interv : interventions.values()){
+            totalTmpsInterv += interv.getDuree();
+        }
+        return  totalTmpsEDT - totalTmpsInterv;
+    }
+
+    public boolean enSousService(){
+        int totalTmpsPrevu = heuresPrevues();
+        if (totalTmpsPrevu < 192){
+            return false;
+        }else{
+            return true;
+        }
     }
 
 }
