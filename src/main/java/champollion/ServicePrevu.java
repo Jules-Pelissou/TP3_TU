e32a37734e6c35afa25f0080ff082254c3ace87e
Jules Pelissou <113369857+Jules-Pelissou@users.noreply.github.com>
2024-11-20 12:11:10 +0100

    Update ServicePrevu.java

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,32 @@
 package champollion;
 
+import lombok.Getter;
+
 public class ServicePrevu {
 	// TODO : implémenter cette classe
+    // Attributs
+    protected int volumeCM;
+    protected int volumeTD;
+    protected int volumeTP;
+    protected UE ue;
 
+    public ServicePrevu(UE ue, int volumeCM, int volumeTD, int volumeTP) {
+        this.ue = ue;
+        this.volumeCM = volumeCM;
+        this.volumeTD = volumeTD;
+        this.volumeTP = volumeTP;
+    }
+
+    public void setVolumeCM(int volumeCM){
+        this.volumeCM = volumeCM;
+    }
+    public void setVolumeTD(int volumeTD){
+        this.volumeTD = volumeTD;
+    }
+    public void setVolumeTP(int volumeTP){
+        this.volumeTP = volumeTP;
+    }
+    public void setUE(UE ue){
+        this.ue = ue;
+    }
 }
